91e0b70a03165bec651d394e3697a382d5b482b8
sirai tooru <zabiisann@gmail.com>
2017-07-12 09:10:01 +0000

    Finish layout and routes

--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
+  get 'help' => 'static_pages#help'
+  get 'logout' => 'static_pages#logout'
+  get 'login' => 'static_pages#login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
 end
