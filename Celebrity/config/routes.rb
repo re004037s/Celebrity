8783ae702f562d66b168456a19d868c80c238060
sirai tooru <zabiisann@gmail.com>
2017-07-15 16:26:48 +0000

    check css

--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get 'static_pages/qa'
   get 'contact' => 'static_pages#contact'
   get 'about' => 'static_pages#about'
   get 'help' => 'static_pages#help'
-  get 'logout' => 'static_pages#logout'
   get 'login' => 'static_pages#login'
-
+  get 'signup' => 'static_pages#signup'
+  get 'logout' => 'static_pages#logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
