811443006d83bb458b4f945920234ec6b38feadb
Ryota Takayama <r.takayama92@gmail.com>
2017-07-17 06:41:27 +0000

    Create forms and Sessions/Users Controller

--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup', to:'users#new'
+
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+
   get 'static_pages/home'
   get 'contact' => 'static_pages#contact'
   get 'about' => 'static_pages#about'
