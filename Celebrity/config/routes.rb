6762c037e5d009a084030782aa3281eb1b9cc7d9
Ryota Takayama <r.takayama92@gmail.com>
2017-07-18 15:39:42 +0000

    PostgreSQLのテスト

--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get 'signup' => 'static_pages#signup'
   get 'logout' => 'static_pages#logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
+  
 end
