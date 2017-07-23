9142190e0755878d5254abef8ebf8bf7206b328e
sirai tooru <zabiisann@gmail.com>
2017-07-23 00:32:15 +0000

    adjust PC template view

--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get 'static_pages/qa'
-  get 'contact' => 'static_pages#contact'
+  get 'progate' => 'static_pages#progate'
   get 'about' => 'static_pages#about'
   get 'help' => 'static_pages#help'
   get 'login' => 'static_pages#login'
