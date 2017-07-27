daabe3d9113e7078c9d1944a86e1f5c1714a8f54
siraitooru <zabiisann@gmail.com>
2017-07-27 13:14:49 +0000

    create feedback form

--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  get '/signup', to:'users#new'
+  # get '/signup', to:'users#new'
 
-  get '/login', to:'sessions#new'
-  post '/login', to:'sessions#create'
-  delete '/logout', to:'sessions#destroy'
+  # get '/login', to:'sessions#new'
+  # post '/login', to:'sessions#create'
+  # delete '/logout', to:'sessions#destroy'
 
   get 'static_pages/home'
-  get 'static_pages/qa'
+  get 'qa' => 'static_pages#qa'
   get 'progate' => 'static_pages#progate'
   get 'about' => 'static_pages#about'
   get 'help' => 'static_pages#help'
   get 'login' => 'static_pages#login'
   get 'signup' => 'static_pages#signup'
   get 'logout' => 'static_pages#logout'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
