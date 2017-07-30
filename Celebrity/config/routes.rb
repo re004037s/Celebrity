8bd968ab56fe67ff0650c0bd153ecce4e7c24893
Ryota Takayama <r.takayama92@gmail.com>
2017-07-30 08:29:50 +0000

    Implement basic login

--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
 
-  get 'static_pages/qa'
-  get 'progate' => 'static_pages#progate'
-  get '/contact' => 'static_pages#contact'
-  get '/about' => 'static_pages#about'
-  get '/help' => 'static_pages#help'
+  get '/qa', to: 'static_pages#qa'
+  get '/progate', to: 'static_pages#progate'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
 
   root 'static_pages#home'
   
