ca05ca20602e9a3e3a0b4c8baa44a47672de4fed
Ryota Takayama <r.takayama92@gmail.com>
2017-08-26 14:40:35 +0000

    Fixed an error

--- Celebrity/app/controllers/application_controller.rb
+++ Celebrity/app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   
   def logged_in_user
     unless logged_in?
-      store_location
       flash[:danger] = 'ログインしてください'
       redirect_to login_url
     end
