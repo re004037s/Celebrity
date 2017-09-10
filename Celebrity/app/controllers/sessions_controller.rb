eb8ec4d5150c5ecd277fcaf88fddc7fd10d59978
Ryota Takayama <r.takayama92@gmail.com>
2017-09-10 05:21:20 +0000

    QAページのリンク追加

--- Celebrity/app/controllers/sessions_controller.rb
+++ Celebrity/app/controllers/sessions_controller.rb
@@ -9,6 +9,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && existence_user?(user) && user.authenticate(params[:session][:password])
       log_in user
+      
+      # ログインユーザに紐付く進捗テーブルレコードの作成（nil回避）
+      HtmlCssStatus.create(user_id: user.id)       if user.html_css_status.nil?
+      JavascriptStatus.create(user_id: user.id)    if user.javascript_status.nil?
+      RubyStatus.create(user_id: user.id)          if user.ruby_status.nil?
+      RubyonrailsStatus.create(user_id: user.id)   if user.rubyonrails_status.nil?
+      RailstutorialStatus.create(user_id: user.id) if user.railstutorial_status.nil?
+      
       redirect_to root_url
     else
       # エラーメッセージを作成する
--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -13,17 +13,15 @@ Rails.application.routes.draw do
   get '/program_learn', to: 'static_pages#program_learn'
   get '/able_engineer', to: 'static_pages#able_engineer'
   get '/job_hunting', to: 'static_pages#job_hunting'
-
   get '/progate', to: 'static_pages#progate'
   post '/progate', to: 'static_pages#progate'
   get '/railstutorial', to: 'static_pages#railstutorial'
+  get '/qa', to: 'static_pages#qa'
   
   patch '/html_css_status', to: 'html_css_statuses#update'
   patch '/javascript_status', to: 'javascript_statuses#update'
   patch '/ruby_status', to: 'ruby_statuses#update'
   patch '/rubyonrails_status', to: 'rubyonrails_statuses#update'
   patch '/railstutorial_status', to: 'railstutorial_statuses#update'
-
-  get '/qa', to: 'static_pages#qa'
 
 end
