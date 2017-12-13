9cb77a0e5b02d8cc9df6d8d38c16b535e6d33c55
hiroki <hiroki10037@gmail.com>
2017-12-13 14:59:35 +0000

    あああ

--- Celebrity/db/schema.rb
+++ Celebrity/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202122748) do
+ActiveRecord::Schema.define(version: 20171213115437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_comments_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "feedbacks", force: :cascade do |t|
     t.text     "feedback"
@@ -196,6 +205,7 @@ ActiveRecord::Schema.define(version: 20171202122748) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "feedbacks", "users"
   add_foreign_key "movies", "movie_categories"
 end
