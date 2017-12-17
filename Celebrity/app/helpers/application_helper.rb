8379931d17581bdf60dc2a2426e3b583f71ac96f
Yuya Nakano <tns.b2g8n@gmail.com>
2017-12-17 08:32:03 +0000

    Progateリンク制御条件追加

--- Celebrity/app/helpers/application_helper.rb
+++ Celebrity/app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
       return false
     end
   end
+  
+  def movie_comp?
+    last_movie = MovieCategory.where(must_view: true).order('sort_order').last.movies.order('sort_order').last
+    if (current_user.feedbacks.find_by(movie_id: last_movie.id) &&
+    current_user.html_css_status.schedule_date &&
+    current_user.javascript_status.schedule_date &&
+    current_user.ruby_status.schedule_date &&
+    current_user.rubyonrails_status.schedule_date)
+      return true
+    else
+      return false
+    end  
+  end    
 end
