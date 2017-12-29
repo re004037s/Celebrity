0e35202c59ce569d74dabc13a9fb09da6d2e6990
Tokoi Kazunori <trustsport12quruli@yahoo.co.jp>
2017-12-29 08:15:04 +0000

    password_reset2

--- Celebrity/app/models/user.rb
+++ Celebrity/app/models/user.rb
@@ -76,12 +76,10 @@ class User < ApplicationRecord
 
     # 有効化トークンとダイジェストを作成および代入する
     def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
     end
     
       # ランダムなトークンを返す
     def User.new_token
-      SecureRandom.urlsafe_base64
+    SecureRandom.urlsafe_base64
     end
 end
--- Celebrity/test/mailers/previews/user_mailer_preview.rb
+++ Celebrity/test/mailers/previews/user_mailer_preview.rb
@@ -2,6 +2,14 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  
+  def account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
+  end
+  
+  
   def password_reset
     user = User.first
     user.reset_token = User.new_token
