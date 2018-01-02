bab4c9d4d6946e57de0e5180612705f41b294731
ryugomikami <mikamiryuugo@yahoo.co.jp>
2018-01-02 14:17:03 +0000

    tag,user_tagテーブルに保存できる

--- Celebrity/app/models/tag.rb
+++ Celebrity/app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
     has_many :user_tags,  foreign_key: 'tag_id'     #user_tags(中間テーブル)を多く持っている has_manyに指定する場合複数形　by本
     has_many :users, through: :user_tags #:throughでuser_tags(中間テーブル)を通じて、userを多く持っている。throughを定義する場合、それに関連するもの通過するものは先に関連付けさせる。
-    accepts_nested_attributes_for :user_tag #他のモデルを一括で更新、保存できるようにするもの
+    # accepts_nested_attributes_for :user_tag #他のモデルを一括で更新、保存できるようにするもの
     # validates :tag, presence: true #タグが空の場合バリデート
     # validates :user_id, presence: true #user_idが存在しているかのバリデート
 end
--- Celebrity/app/models/user.rb
+++ Celebrity/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     # acts_as_taggable
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save { self.email = email.downcase }
     default_scope -> { order(:created_at) }
     mount_uploader :picture, PictureUploader #画像アップロード用に追加
@@ -24,7 +25,7 @@ class User < ApplicationRecord
     has_many :feedbacks
     has_many :user_tags, foreign_key: 'user_id' #user_tagを多く持っている,コメントを削除する動作。has_manyに指定する場合複数形
     has_many :tags, through: :user_tags #:throughでuser_tags(中間テーブル)を通じて、tagを多く持っている.throughを定義する場合、それに関連するもの通過するものは先に関連付けさせる。
-    accepts_nested_attributes_for :user_tags, allow_destroy: true #関連項目も含めて一度に保存、削除するとい意味
+    # accepts_nested_attributes_for :user_tags, allow_destroy: true #関連項目も含めて一度に保存、削除するとい意味
     
     # 渡された文字列のハッシュ値を返す
     def User.digest(string)
@@ -60,4 +61,54 @@ class User < ApplicationRecord
     end
     end
     
+    # アカウントを有効にする
+   def activate
+     update_attribute(:activated,    true)
+     update_attribute(:activated_at, Time.zone.now)
+   end
+ 
+   # 有効化用のメールを送信する
+   def send_activation_email
+     UserMailer.account_activation(self).deliver_now
+   end
+ 
+   # パスワード再設定の属性を設定する
+   def create_reset_digest
+     self.reset_token = User.new_token
+     update_attribute(:reset_digest,  User.digest(reset_token))
+     update_attribute(:reset_sent_at, Time.zone.now)
+   end
+   
+    # パスワード再設定のメールを送信する
+   def send_password_reset_email
+     UserMailer.password_reset(self).deliver_now
+   end
+   # パスワード再設定の期限が切れている場合はtrueを返す
+   def password_reset_expired?
+     reset_sent_at < 2.hours.ago
+   end
+   
+   # トークンがダイジェストと一致したらtrueを返す
+   def authenticated?(attribute, token)
+     digest = send("#{attribute}_digest")
+     return false if digest.nil?
+     BCrypt::Password.new(digest).is_password?(token)
+   end
+ 
+   private
+ 
+     # メールアドレスをすべて小文字にする
+    def downcase_email
+       self.email = email.downcase
+    end
+ 
+     # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+    end
+      
+       # ランダムなトークンを返す
+    def User.new_token
+       SecureRandom.urlsafe_base64
+    end
+   
 end
