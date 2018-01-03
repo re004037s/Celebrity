9008951ed50206798691442cc9e59f8c614b9808
osamuhiraishi <osm890221@gmail.com>
2018-01-03 15:18:30 +0000

    新規投稿、編集、削除機能の実装

--- Celebrity/app/controllers/qiita_posts_controller.rb
+++ Celebrity/app/controllers/qiita_posts_controller.rb
@@ -1,55 +1,56 @@
 class QiitaPostsController < ApplicationController
-  # before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
   
   def index
-    @qiitaposts = QiitaPost.all
-  end
-  
-  def show
-    # @user = User.find(params[:id])
-    @qiitaposts = QiitaPost.find(params[:id])
+    @qiitaposts = QiitaPost.all.page(params[:page])
   end
   
   def new
-    @qiitaposts = QiitaPost.new
+    @qiitapost = QiitaPost.new
   end
   
   def create
-    @qiitaposts = current_user.qiita_posts.build(qiitapost_params)
-    if @qiitaposts.save
+    @qiitapost = current_user.qiita_posts.build(qiitapost_params)
+    if @qiitapost.save
       flash[:success] = "Qiitaが投稿されました"
-      redirect_to @qiitaposts
+      redirect_to qiita_posts_url
     else
       render 'new'
     end
   end
   
   def edit
-    @qiitaposts = QiitaPost.find(params[:id])
+    @qiitapost = QiitaPost.find(params[:id])
   end
   
   def update
-    @qiitaposts = QiitaPost.find(params[:id])
-    @qiitaposts.attributes = params[:qiita_post]
-    if @qiitaposts.save
+    @qiitapost = QiitaPost.find(params[:id])
+    if @qiitapost.update_attributes(qiitapost_params)
       flash[:success] = "Qiitaが更新されました"
-      redirect_to @qiitaposts
+      redirect_to qiita_posts_url
     else
       render 'edit'
     end
   end
 
   def destroy
-    @qiitaposts = QiitaPost.find(params[:id])
-    @qiitaposts.destroy
-    redirect_to @qiitaposts
+    # @qiitapost = QiitaPost.find(params[:id])
+    @qiitapost.destroy
+    flash[:success] = "Qiitaが削除されました"
+    redirect_to qiita_posts_url
   end
 
-
+  
   private
 
     def qiitapost_params
       params.require(:qiita_post).permit(:title, :url)
+    end
+    
+    def correct_user
+      @qiitapost = current_user.qiita_posts.find_by(id: params[:id])
+      redirect_to qiita_posts_url if @qiitapost.nil?
     end
 end
   
--- Celebrity/config/routes.rb
+++ Celebrity/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
   
+  
+  
   resources :users do
       member do
         get 'get_image'
