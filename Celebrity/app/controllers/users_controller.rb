5ba92518901b6ec31fa5ab9bc61b188c66381b8c
ryugomikami <mikamiryuugo@yahoo.co.jp>
2018-01-08 09:35:17 +0000

    複数エラーflashの表示

--- Celebrity/app/controllers/users_controller.rb
+++ Celebrity/app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
       redirect_to @user 
     else
     
-    flash[:error] = tags
+    flash[:danger] = tag.errors.messages[:tag].join(' / ') ##[:tag]で配列を取り出し、.join(' / ')で分割した。
     redirect_to @user
     # end
     end
