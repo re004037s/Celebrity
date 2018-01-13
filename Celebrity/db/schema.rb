e321f321d28087e77892b90289a4263841e7025c
com <com10281028@gmail.com>
2018-01-13 07:39:08 +0000

    LineID入力項目追加

--- Celebrity/db/schema.rb
+++ Celebrity/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103225219) do
+ActiveRecord::Schema.define(version: 20180113061019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -200,12 +200,27 @@ ActiveRecord::Schema.define(version: 20180103225219) do
     t.index ["user_id"], name: "index_rubyonrails_statuses_on_user_id", using: :btree
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.text     "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "user_movie_statuses", force: :cascade do |t|
     t.integer  "user_id"
     t.date     "schedule_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.index ["user_id"], name: "index_user_movie_statuses_on_user_id", using: :btree
+  end
+
+  create_table "user_tags", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_user_tags_on_tag_id", using: :btree
+    t.index ["user_id"], name: "index_user_tags_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -222,6 +237,7 @@ ActiveRecord::Schema.define(version: 20180103225219) do
     t.binary   "picture_file"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "line_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
@@ -229,4 +245,6 @@ ActiveRecord::Schema.define(version: 20180103225219) do
   add_foreign_key "feedbacks", "users"
   add_foreign_key "movies", "movie_categories"
   add_foreign_key "qiita_posts", "users"
+  add_foreign_key "user_tags", "tags"
+  add_foreign_key "user_tags", "users"
 end
