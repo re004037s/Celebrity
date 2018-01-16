7ee6d02ab74966b2c0dd82ae8f784854c9ee652c
ryugomikami <mikamiryuugo@yahoo.co.jp>
2018-01-16 16:33:11 +0000

    追加 color column & icon column to tags table

--- Celebrity/db/schema.rb
+++ Celebrity/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223063115) do
+ActiveRecord::Schema.define(version: 20180116153032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,6 +178,8 @@ ActiveRecord::Schema.define(version: 20171223063115) do
     t.text     "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "color"
+    t.binary   "icon"
   end
 
   create_table "user_movie_statuses", force: :cascade do |t|
