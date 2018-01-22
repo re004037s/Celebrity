ccb2cdf8d535810231a97d98e109dffa31ed4dc6
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-01-22 10:50:43 +0000

    メール本文のファイル名変更

--- Celebrity/config/environments/development.rb
+++ Celebrity/config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://celebrity-engineer-site-com1028.c9users.io'  # 開発環境のhost名に合わせる 
+  host = 'https://celebrity-engineer-pigaru8966.c9users.io'  # 開発環境のhost名に合わせる 
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
