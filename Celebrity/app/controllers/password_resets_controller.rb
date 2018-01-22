d7b08c7a83a8c9b32a79fdbcfb1c41ed16cb924e
Haru <capybara1229@gmail.com>
2018-01-22 15:24:49 +0000

    エラー解決（パスワード再設定エラー時のページ遷移）

--- Celebrity/app/controllers/password_resets_controller.rb
+++ Celebrity/app/controllers/password_resets_controller.rb
@@ -15,8 +15,10 @@ class PasswordResetsController < ApplicationController
       flash[:info] = "パスワード再設定用のメールが送信されました。"
       redirect_to login_url
     else
-      flash.now[:danger] = "メールアドレスが無効です。"
-      render 'new'
+      # flash.now[:danger] = "メールアドレスが無効です。"
+      # render 'new'
+      flash[:danger] = "メールアドレスが無効です。"
+      redirect_to login_url
     end
   end
 
