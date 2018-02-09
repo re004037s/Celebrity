4a4843b810a2f8305888476d42dc0cf584c79cb6
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-02-09 12:55:01 +0000

    アクションを編集

--- Celebrity/app/controllers/skillsheet_controller.rb
+++ Celebrity/app/controllers/skillsheet_controller.rb
@@ -1,16 +1,31 @@
 class SkillsheetController < ApplicationController
   
   def index
-    @user = current_user
+    @iuser = current_user
   end
   
+  
+  def new
+    @skillsheet = Skillsheet.new
+    @skillsheet.name = ""
+  end
+
   def create
+    @skillsheet = Skillsheet.new(skillsheet_paprams)
+    @skillsheet.name = params[:skillsheet][:excel].original_filename
+    @skillsheet.picture = params[:skillsheet][:excel].read
+
+    if @skillsheet.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
-  
-  def destroy
-  end
-  
-  def edit
-  end
-  
+
+  private
+
+    def skillsheet_paprams
+      params.require(:image).permit(:name, :picture)
+    end
+
 end
