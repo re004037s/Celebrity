334e1ea828b1c5b808bb7a2e844f6232d3c12172
Yuichi Kubo <newstart1ine.yk@gmail.com>
2018-02-09 00:43:24 +0000

    add text validation

--- Celebrity/app/models/pdca_post.rb
+++ Celebrity/app/models/pdca_post.rb
@@ -1,4 +1,10 @@
 class PdcaPost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
+  with_options presence: true do
+  validates :plan
+  validates :do
+  validates :check
+  validates :action
+  end
 end
