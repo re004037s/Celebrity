cd4101ee944619b486b51bf76b212817fa7f5075
Yuichi Kubo <newstart1ine.yk@gmail.com>
2018-02-09 00:47:19 +0000

    add default_scope in pdca_post.rb

--- Celebrity/app/models/pdca_post.rb
+++ Celebrity/app/models/pdca_post.rb
@@ -1,5 +1,6 @@
 class PdcaPost < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   with_options presence: true do
   validates :plan
