413148a8d74218ba1461defd3469f1fdda4bf400
hiroki <hiroki10037@gmail.com>
2018-02-17 06:15:59 +0000

    profile_image

--- Celebrity/app/uploaders/picture_uploader.rb
+++ Celebrity/app/uploaders/picture_uploader.rb
@@ -11,6 +11,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # アップロードファイルの保存先ディレクトリは上書き可能
   # 下記はデフォルトの保存先  
   def store_dir
+    
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
