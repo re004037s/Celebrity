e6d4b34f74661efeea9aa2437fc09c55faf87500
Soichi Kamiya <kamiyasoichi@hotmail.co.jp>
2018-03-09 14:31:29 +0000

    before_action追加

--- Celebrity/app/controllers/static_pages_controller.rb
+++ Celebrity/app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   before_action :logged_in_user
   before_action :setting
   before_action :set_movie_categories
+  before_action :comp_portfolio, only: :portfolio_mv
+  before_action :comp_progate, only: :progate
+  before_action :comp_railstutorial, only: :railstutorial
   
   def qa
   end
@@ -42,6 +45,34 @@ class StaticPagesController < ApplicationController
     
     def set_movie_categories
       @categories_all = MovieCategory.all.order('sort_order')
+    end
+    
+    def comp_portfolio
+      if current_user.railstutorial_status.nil?
+      else 
+        flash[:danger] = "先に Rails Tutorial を完了させて下さい"
+        redirect_to root_url
+      end
+    end
+    
+    def comp_progate
+      if Feedback.where(user_id: current_user).count == 
+         Movie.where(movie_category_id: MovieCategory.where(must_view: true).ids).count
+      else
+        flash[:danger] = "先に動画を視聴して下さい"
+        redirect_to root_url
+      end
+    end
+    
+    def comp_railstutorial
+      if current_user.html_css_status.ji_2 \
+        && current_user.javascript_status.do_1 \
+        && current_user.ruby_status.ga_5 \
+        && current_user.rubyonrails_status.do_4
+      else
+        flash[:danger] = "先に Progate を完了させて下さい"
+        redirect_to root_url
+      end
     end
     
 end
