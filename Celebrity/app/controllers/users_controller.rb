e66ae90733d4a9d17537f7b3a61448b949133825
com <com10281028@gmail.com>
2018-03-15 14:42:38 +0000

    追加 ステータス決定ボタン・ステータス検索ボタン

--- Celebrity/app/controllers/users_controller.rb
+++ Celebrity/app/controllers/users_controller.rb
@@ -59,6 +59,12 @@ class UsersController < ApplicationController
     
   end
   
+  #ステータス（未登録・営業中・就業中）を登録する
+  def register_status
+    @status = User.new(params.require(:sutatus).permit(:status))
+    @status.save
+  end
+  
   def new
     @user = User.new
   end
