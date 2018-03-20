f82fdfbd6788a67d09a53951d4c4b071d253fe01
MasahikoYonezawa <y.masahiko0802@gmail.com>
2018-03-20 15:08:54 +0000

    バリデーションの修正

--- Celebrity/app/models/comment.rb
+++ Celebrity/app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 200 }
 end
--- Celebrity/app/models/qiita_post.rb
+++ Celebrity/app/models/qiita_post.rb
@@ -2,6 +2,6 @@ class QiitaPost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :title, presence: true
-  validates :url, presence: true
+  validates :title, presence: true, length: { maximum: 25 }
+  validates :url, presence: true, length: { maximum: 100 }
 end
