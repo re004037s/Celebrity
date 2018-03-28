aba8809f1e24d6817649d563761a48bc35b08470
Yuichi Kubo <newstart1ine.yk@gmail.com>
2018-03-28 00:02:43 +0000

    fixed

--- Celebrity/db/schema.rb
+++ Celebrity/db/schema.rb
@@ -66,13 +66,6 @@ ActiveRecord::Schema.define(version: 20180313130347) do
     t.index ["user_id"], name: "index_html_css_statuses_on_user_id", using: :btree
   end
 
-  create_table "information", force: :cascade do |t|
-    t.text     "info"
-    t.date     "display_period"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
   create_table "interview_posts", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
@@ -319,7 +312,7 @@ ActiveRecord::Schema.define(version: 20180313130347) do
     t.datetime "reset_sent_at"
     t.string   "line_id"
     t.binary   "skillsheet"
-    t.string   "skillsheet_name"
+    t.string   "skillsheet_name", default: "未登録"
     t.string   "status",          default: "未登録"
     t.boolean  "guest",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
