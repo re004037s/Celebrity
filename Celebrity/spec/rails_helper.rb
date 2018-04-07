daacd5597584cbc4c6ddaf1e8d03e3c672a4382a
yuta-ushijima <register@yuta-u.com>
2018-04-07 14:59:27 +0000

    specを使用するためにrails_helper.rbへ設定を追記

--- Celebrity/spec/rails_helper.rb
+++ Celebrity/spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'capybara/poltergeist'
+require 'factory_girl_rails'
+require 'capybara/rspec'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -33,7 +37,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -54,4 +58,35 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+
+  #requireしたことで使えるメソッドで、capybaraテストでdeviseメソッドを使えるようにしている
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  #factory_botのメソッドを使うためのもの
+  config.include FactoryBot::Syntax::Methods
+  #capybaraでJSテストをできるようにするため
+  Capybara.javascript_driver = :poltergeist
+  # capybaraでテストするアプリケーションサーバーにpumaを指定
+  Capybara.server = :puma
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
