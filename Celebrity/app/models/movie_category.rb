3359558dc21f7093a4a9a6201ca5b7286f31b2b5
Yuichi Kubo <newstart1ine.yk@gmail.com>
2018-04-16 05:35:44 +0000

    fixed bug

--- Celebrity/app/models/movie_category.rb
+++ Celebrity/app/models/movie_category.rb
@@ -1,6 +1,6 @@
 class MovieCategory < ApplicationRecord
   include SessionsHelper
   acts_as_paranoid
-  has_many :movies
+  has_many :movies, dependent: :destroy
   validates :name, presence: true, length: { maximum: 12 }
 end
