fbd01bfb516e30b3f3837f3d2cc5b378defbc2f5
yuki hamamoto <chachacb650f@gmail.com>
2018-04-27 15:31:06 +0000

    不要なカラムを削除

--- Celebrity/db/migrate/20180422095637_add_category_to_qestions.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToQestions < ActiveRecord::Migration[5.0]
-  def change
-    add_column :questions, :category, :string
-  end
-end
--- Celebrity/db/schema.rb
+++ Celebrity/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422095637) do
+ActiveRecord::Schema.define(version: 20180427140905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -163,7 +163,6 @@ ActiveRecord::Schema.define(version: 20180422095637) do
     t.text     "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "category"
   end
 
   create_table "railstutorial_statuses", force: :cascade do |t|
