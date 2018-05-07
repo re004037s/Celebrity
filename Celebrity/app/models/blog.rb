2fa5b32da9383766e12db695543db57d5ba248a7
MasahikoYonezawa <y.masahiko0802@gmail.com>
2018-05-07 12:48:02 +0000

    不要ファイルを削除し文字数制限追加

--- Celebrity/app/models/blog.rb
+++ Celebrity/app/models/blog.rb
@@ -1,7 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 3000 }
   validates :thumbnail, presence: true
   validate  :picture_size
   mount_uploader :thumbnail, ThumbnailUploader
