0fe2ed31336572950534cae23c9a7e3a83fb060d
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-05-11 10:54:18 +0000

    イベント属性入力フォームでエラー

--- Celebrity/db/schema.rb
+++ Celebrity/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508084744) do
+ActiveRecord::Schema.define(version: 20180511102533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 20180508084744) do
     t.datetime "updated_at", null: false
     t.date     "day"
     t.string   "picture"
+    t.boolean  "type"
+    t.boolean  "free"
+    t.boolean  "venture"
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
