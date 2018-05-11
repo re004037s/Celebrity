d3746db93a3613e76772c30d767b525b38d28f21
yuki hamamoto <chachacb650f@gmail.com>
2018-05-11 17:14:13 +0000

    インデックスページのカテゴリーと質問表示完了

--- Celebrity/app/controllers/question_categories_controller.rb
+++ Celebrity/app/controllers/question_categories_controller.rb
@@ -10,28 +10,28 @@ class QuestionCategoriesController < ApplicationController
     
     def create
         @question_categories = QuestionCategory.new(question_category_params)
-            if @question_categories.save
-                flash[:success] = "作成しました"
-                redirect_to questions_url
-            else
-                flash[:danger] = "再度入力して下さい"
-                redirect_to :back
-            end
+        if @question_categories.save
+            flash[:success] = "作成しました"
+            redirect_to questions_url
+        else
+            flash[:danger] = "再度入力して下さい"
+            redirect_to :back
+        end
     end
     
     def edit
-                @question_categories = QuestionCategory.find(params[:id])
+        @question_categories = QuestionCategory.find(params[:id])
     end
     
     def update
-                @question_categories = QuestionCategory.find(params[:id])
-            if @question_categories.update_attributes(question_category_params)
-                flash[:success] = "更新しました"
-                redirect_to questions_url
-            else
-                flash[:danger] = "再度入力して下さい"
-                redirect_to :back
-            end
+        @question_categories = QuestionCategory.find(params[:id])
+        if @question_categories.update_attributes(question_category_params)
+            flash[:success] = "更新しました"
+            redirect_to questions_url
+        else
+            flash[:danger] = "再度入力して下さい"
+            redirect_to :back
+        end
     end
     
     def destroy
