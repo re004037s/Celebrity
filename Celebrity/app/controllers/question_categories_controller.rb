aa606f5d9a57d33ec842fb89d0283ffa87aa1594
yuki hamamoto <chachacb650f@gmail.com>
2018-05-13 02:00:35 +0000

    スペースタグ修正4から2へ

--- Celebrity/app/controllers/question_categories_controller.rb
+++ Celebrity/app/controllers/question_categories_controller.rb
@@ -1,50 +1,50 @@
 class QuestionCategoriesController < ApplicationController
-    before_action :admin_user, only:[:index, :create, :edit, :delete ]
-    
-    def index
+  before_action :admin_user, only:[:index, :create, :edit, :delete ]
+  
+  def index
+  end
+  
+  def new
+    @question_categories = QuestionCategory.new
+  end
+  
+  def create
+    @question_categories = QuestionCategory.new(question_category_params)
+    if @question_categories.save
+      flash[:success] = "作成しました"
+      redirect_to questions_url
+    else
+      flash[:danger] = "再度入力して下さい"
+      redirect_to :back
     end
-    
-    def new
-        @question_categories = QuestionCategory.new
+  end
+  
+  def edit
+    @question_categories = QuestionCategory.find(params[:id])
+  end
+  
+  def update
+    @question_categories = QuestionCategory.find(params[:id])
+    if @question_categories.update_attributes(question_category_params)
+      flash[:success] = "更新しました"
+      redirect_to questions_url
+    else
+      flash[:danger] = "再度入力して下さい"
+      redirect_to :back
     end
-    
-    def create
-        @question_categories = QuestionCategory.new(question_category_params)
-        if @question_categories.save
-            flash[:success] = "作成しました"
-            redirect_to questions_url
-        else
-            flash[:danger] = "再度入力して下さい"
-            redirect_to :back
-        end
-    end
-    
-    def edit
-        @question_categories = QuestionCategory.find(params[:id])
-    end
-    
-    def update
-        @question_categories = QuestionCategory.find(params[:id])
-        if @question_categories.update_attributes(question_category_params)
-            flash[:success] = "更新しました"
-            redirect_to questions_url
-        else
-            flash[:danger] = "再度入力して下さい"
-            redirect_to :back
-        end
-    end
-    
-    def destroy
-                        @question_categories = QuestionCategory.find(params[:id])
-                @question_categories.destroy
-        flash[:success] = "削除されました"
-        redirect_to questions_url
-    end
-    
-    private
-    
-    def question_category_params
-        params.require(:question_category).permit(:name)
-    end
-    
+  end
+  
+  def destroy
+    @question_categories = QuestionCategory.find(params[:id])
+    @question_categories.destroy
+    flash[:success] = "削除されました"
+    redirect_to questions_url
+  end
+  
+  private
+  
+  def question_category_params
+    params.require(:question_category).permit(:name)
+  end
+  
 end
--- Celebrity/app/controllers/questions_controller.rb
+++ Celebrity/app/controllers/questions_controller.rb
@@ -2,40 +2,40 @@ class QuestionsController < ApplicationController
     before_action :admin_user, only:[:index, :create, :edit, :delete ]
     
     def index
-        @questions = Question.all.page(params[:page])
-        @question_categories = QuestionCategory.all
+      @questions = Question.all.page(params[:page])
+      @question_categories = QuestionCategory.all
     end
 
 
     def new
-        @question = Question.new 
+      @question = Question.new 
     end
 
     def create
-        @question = Question.create(question_params)
-            if @question.save
-                flash[:success] = "作成しました"
-                redirect_to questions_url
-            else
-                flash[:danger] = "再度入力して下さい"
-                redirect_to :back
-            end
+      @question = Question.create(question_params)
+        if @question.save
+          flash[:success] = "作成しました"
+          redirect_to questions_url
+        else
+          flash[:danger] = "再度入力して下さい"
+          redirect_to :back
+        end
     end
 
 
     def edit
-        @question = Question.find(params[:id])
+      @question = Question.find(params[:id])
     end
     
     def update
-        @question = Question.find(params[:id])
-            if @question.update_attributes(question_params)
-                flash[:success] = "更新しました"
-                redirect_to questions_url
-            else
-                flash[:danger] = "再度入力して下さい"
-                redirect_to :back
-            end
+      @question = Question.find(params[:id])
+        if @question.update_attributes(question_params)
+          flash[:success] = "更新しました"
+          redirect_to questions_url
+        else
+          flash[:danger] = "再度入力して下さい"
+          redirect_to :back
+        end
     end
     
     
@@ -50,11 +50,11 @@ class QuestionsController < ApplicationController
  private
  
     def question_params
-        params.require(:question).permit(:question, :answer, :category_id)
+      params.require(:question).permit(:question, :answer, :category_id)
     end
     
     def question_category_params
-        params.require(:question_category).permit(:name)
+      params.require(:question_category).permit(:name)
     end
     
 end
