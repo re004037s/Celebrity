2d4a80cbcc7ed10a1f4096e98244f842e4998ea1
MasahikoYonezawa <y.masahiko0802@gmail.com>
2018-05-22 14:25:15 +0000

    とりあえずコミット

--- Celebrity/db/migrate/20180504024140_create_events.rb
+++ Celebrity/db/migrate/20180504024140_create_events.rb
@@ -6,6 +6,6 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.text :text
       t.timestamps
     end
-          add_index :events, [:user_id, :created_at]
+          #add_index :events, [:user_id, :created_at]
   end
 end
