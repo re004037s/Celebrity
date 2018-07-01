380b065308ba0c43e52b3793212fa062e27bc2d7
Yuichi Kubo <newstart1ine.yk@gmail.com>
2018-07-01 13:09:55 +0000

    fixed a little

--- Celebrity/db/schema.rb
+++ Celebrity/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701110409) do
+ActiveRecord::Schema.define(version: 20180701130739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -351,7 +351,7 @@ ActiveRecord::Schema.define(version: 20180701110409) do
     t.boolean  "guest",              default: false
     t.boolean  "venture_user",       default: false, null: false
     t.boolean  "free_engineer_user", default: true,  null: false
-    t.boolean  "staff_user",         default: true,  null: false
+    t.boolean  "staff_user",         default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
