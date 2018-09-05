215517452799a1ead073262e1d8d3214578c68a6
Yuichi Kubo <newstart1ine.yk@gmail.com>
2018-09-05 23:18:55 +0000

    add dotenv

--- Celebrity/config/environments/production.rb
+++ Celebrity/config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'smtp.gmail.com',
-    user_name: 'celebrityengineer.members@gmail.com',
-    password: 'istttnljpomaisjo',
+    user_name: ENV['USER_NAME'],
+    password: ENV['PASSWORD'],
     authentication: 'login',
     enable_starttls_auto: true
   }
