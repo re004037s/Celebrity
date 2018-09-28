20baec8cab8b90f690370627d4866e343e42234a
Soichi Kamiya <kamiyasoichi@hotmail.co.jp>
2018-09-28 16:00:01 +0000

    レイアウト調整

--- Celebrity/app/controllers/faq_controller.rb
+++ Celebrity/app/controllers/faq_controller.rb
@@ -1,15 +1,20 @@
 class FaqController < ApplicationController
-  include ApplicationHelper
+  before_action :logged_in_user, only: [:index]
+  before_action :admin_user, only: [:create, :update, :destroy]
   
   def index
-    @faqs = Question.all.order('created_at ASC')
     @faq = Question.new
+    @faqs1 = Question.where(id: [1])
+    @faqs2_4 = Question.where(id: [2, 3, 4])
+    @faqs5_8 = Question.where(id: [5, 6, 7, 8])
+    @faqs9 = Question.where(id: [9])
+    @faqs = Question.where("id > ?", 9)
   end
 
   def create
    @faq = Question.new(faq_params)
     if @faq.save
-      flash[:success] = "質問が登録されました"
+      flash[:success] = "質問が作成されました"
       redirect_to faq_index_path
     else
       render 'index'
@@ -19,7 +24,7 @@ class FaqController < ApplicationController
   def update
     @faq = Question.find(params[:id])
     if @faq.update_attributes(faq_params)
-      flash[:success] = "内容が更新されました"
+      flash[:success] = "内容が変更されました"
       redirect_to faq_index_path
     else
       render 'index'
