fef1540d50a8666028b8aa30d2cd863b407ccf77
Akira Haruumi <capybara1229@gmail.com>
2018-09-28 13:12:24 +0900

    formatted user_mailer.rb code.

--- Celebrity/app/mailers/user_mailer.rb
+++ Celebrity/app/mailers/user_mailer.rb
@@ -1,30 +1,30 @@
 class UserMailer < ApplicationMailer
 
-  def alert_completion_date(user,no_complete_tasks)		
-    		
-    @user = user		
- 		
-    #user name		
-    @user_name = @user.name		
-  		  
-    #task names		 
+  def alert_completion_date(user, no_complete_tasks)
+    @user = user
+    @user_name  = @user.name
     @task_names = no_complete_tasks
-  
-    #宛先アドレス		 
-    mail to: @user.email, subject: "セレブエンジニアサロンのタスクについて"		
+    mail to: @user.email, subject: "セレブエンジニアサロンのタスクについて"
   end
-  
-  def send_when_create(users,movie)
-       @new_movie = movie
-       mail to: 'test@example.com', subject:  'セレブエンジニアサロンの新規動画について',bcc: users.map{|u| u.email}
+
+
+  def send_when_create(users, movie)
+    @new_movie = movie
+    mail to: 'test@example.com', subject: 'セレブエンジニアサロンの新規動画について',bcc: users.map{|u| u.email}
   end
-  
-  
-  
+
+
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end
+
+
+  def slack_invitation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+  end
+
 
   def password_reset(user)
     @user = user
