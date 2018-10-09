a84c098d61f1508fb44771449144a4f16b33cba4
Soichi Kamiya <kamiyasoichi@hotmail.co.jp>
2018-10-09 14:15:07 +0000

    動画投稿のエラー修正

--- Celebrity/app/mailers/user_mailer.rb
+++ Celebrity/app/mailers/user_mailer.rb
@@ -14,12 +14,18 @@ class UserMailer < ApplicationMailer
     mail to: @user.email, subject: "セレブエンジニアサロンのタスクについて"		
   end
   
-  def send_when_create(users,movie)
-       @new_movie = movie
-       mail to: 'test@example.com', subject:  'セレブエンジニアサロンの新規動画について',bcc: users.map{|u| u.email}
+  def send_when_create(users,movie,i)
+    #SMTP経由のメール1通で送れる最大宛先数は99件
+    max_send_number = 99
+    @new_movie = movie
+    if i == 0
+      mail to: 'kamiyasoichi@hotmail.co.jp', subject:  'セレブエンジニアサロンの新規動画について',
+      bcc: users.first(max_send_number).map{|u| u.email}
+    else
+      mail to: 'kamiyasoichi@hotmail.co.jp', subject:  'セレブエンジニアサロンの新規動画について',
+      bcc: users[(max_send_number*i)..(max_send_number*(i+1))-1].map{|u| u.email}
+    end
   end
-  
-  
   
   def account_activation(user)
     @user = user
