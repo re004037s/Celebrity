de4a4687a23f2101ab0a36d11994afbf2a1121e4
capybara1229 <capybara1229@gmail.com>
2018-10-19 20:57:52 +0900

    Delete init.sh

--- docker/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose run app bundle install
-docker-compose build
-docker-compose run app npm install
-docker-compose run app npm audit fix
-docker-compose up -d
-docker-compose run app bundle exec rake db:setup
